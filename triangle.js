b836baf365e5651867fe9b9856b9405a10f03ce2
shiro <145122334+shiro72@users.noreply.github.com>
2023-11-07 22:55:09 +0300

    Add files via upload

---
+++ triangle.js
@@ -0,0 +1,51 @@
+function verify() {
+    console.log("X, Y, Z")
+    let X = parseFloat(elementX.value);
+    let Y = parseFloat(elementY.value);
+    let Z = parseFloat(elementZ.value);
+    console.log(X, Y, Z)
+
+    if (X * X + Y * Y === Z * Z || X * X + Z * Z === Y * Y || Y * Y + Z * Z === X * X) {
+        result = "Треугольник существует и является прямоугольным"
+        document.getElementById("result").innerText =  result;
+    } else {
+        result = "Треугольник не существует или не является прямоугольным"
+        document.getElementById("result").innerText =  result;
+    }
+}
+
+function send() {
+    if (check) {
+        let textCondition = document.getElementsByTagName('p')[0].innerText
+        document.getElementsByid('formulation')[0].value = textCondition;
+        document.getElementsById('result')[0].value = result;
+        document.getElementById("UserEnter").submit();
+    } else {
+        alert("Ошибка")
+    }
+}
+
+function verify_send() {
+    verify();
+    send();
+}
+
+let messageText = document.getElementById("result").innerText
+console.log(messageText)
+let result;
+let check = false;
+
+const elementX = document.getElementById("X");
+elementX.addEventListener('input', verify);
+
+const elementY = document.getElementById("Y");
+elementY.addEventListener('keyup', verify);
+
+const elementZ = document.getElementById("Z");
+elementZ.addEventListener('keyup', verify);
+
+const elementVerify = document.getElementById("verify");
+elementVerify.addEventListener('click', verify);
+
+const elementSend = document.getElementById("send");
+elementSend.addEventListener('click', send)
